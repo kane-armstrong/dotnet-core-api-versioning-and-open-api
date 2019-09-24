79429b6aceb97a141df494a11a48dd90f1aef9a5
Kane Armstrong <kane.armstrong@outlook.com>
2019-09-24 14:03:08 +1200

    Adjust document version configuration

--- NSwag/Startup.cs
+++ NSwag/Startup.cs
@@ -1,11 +1,13 @@
+using System.Linq;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using NSwag.Generation.Processors.Security;
 
 namespace NSwag
 {
@@ -29,18 +31,61 @@ namespace NSwag
             }).AddVersionedApiExplorer(options =>
             {
                 options.SubstituteApiVersionInUrl = true;
-            }).AddOpenApiDocument(document =>
+            });
+
+            /*
+                This approach looks nice, but explicitly calling AddOpenApiDocument for each version may be preferable 
+                in that it makes it easier to maintain version-specific document configuration. Perhaps v1 is an 
+                unauthenticated API, v2 introduces basic auth, and v3 uses a JWT issued by an OAuth2.0 or OIDC identity 
+                provider. In this scenario, the version description enumeration approach is counterproductive.
+             
+                var versionDescriptionProvider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+                foreach (var versionDescription in versionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    services.AddOpenApiDocument(document =>
+                    {
+                        document.DocumentName = versionDescription.GroupName;
+                        document.ApiGroupNames = new[] {versionDescription.ApiVersion.ToString()};
+                        document.Title = "Weather Forecast API";
+                        document.Description = "This API returns weather forecast information";
+                        document.IgnoreObsoleteProperties = true;
+                        document.SerializerSettings = new JsonSerializerSettings
+                            {ContractResolver = new DefaultContractResolver()};
+                    });
+                }
+             */
+            
+            services.AddOpenApiDocument(document =>
             {
                 document.DocumentName = "v1";
                 document.ApiGroupNames = new[] {"1"};
+                document.Title = "Weather Forecast API";
+                document.Description = "This API returns weather forecast information";
+                document.Version = "v1";
                 document.IgnoreObsoleteProperties = true;
-                document.SerializerSettings = new JsonSerializerSettings { ContractResolver = new DefaultContractResolver() };
+                document.SerializerSettings = new JsonSerializerSettings
+                    {ContractResolver = new DefaultContractResolver()};
             }).AddOpenApiDocument(document =>
             {
                 document.DocumentName = "v2";
                 document.ApiGroupNames = new[] {"2"};
+                document.Title = "Weather Forecast API";
+                document.Description = "This API returns weather forecast information";
+                document.Version = "v1";
                 document.IgnoreObsoleteProperties = true;
-                document.SerializerSettings = new JsonSerializerSettings { ContractResolver = new DefaultContractResolver() };
+                document.SerializerSettings = new JsonSerializerSettings
+                    {ContractResolver = new DefaultContractResolver()};
+                
+                document.OperationProcessors.Add(new OperationSecurityScopeProcessor("JWT"));
+                document.AddSecurity("JWT", Enumerable.Empty<string>(),
+                    new OpenApiSecurityScheme()
+                    {
+                        Type = OpenApiSecuritySchemeType.ApiKey,
+                        Name = nameof(Authorization),
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Description = "Field should be in this format: \nBearer {my token}"
+                    }
+                );
             });
         }
 
